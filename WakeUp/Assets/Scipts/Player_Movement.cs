dbcd83cd22399478dd933b12fd0ecfe6bb5c6fd5
HiZtoria <60591442+HiZtoria@users.noreply.github.com>
2020-02-14 22:40:35 +0800

    GGehh
    Weehh

--- WakeUp/Assets/Scipts/Player_Movement.cs
+++ WakeUp/Assets/Scipts/Player_Movement.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int jumpPower = 1250;
+    private float moveX;
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+       
     }
+
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+        //Animations
+        //direction
+        if (moveX < 0.0f && facingRight == false) {
+            FlipPlayer();
+        } else if (moveX > 0.0f && facingRight == true) {
+            FlipPlayer();
+        }
+        //p6
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+    }
+
+    void FlipPlayer() 
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+
+    }
+
 }
